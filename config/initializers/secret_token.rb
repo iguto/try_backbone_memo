6c17caf835e24d626fa669e4f1add56467755207
ookawa <clc.iguto@gmail.com>
2014-04-28 21:55:22 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MemoBackbone::Application.config.secret_key_base = '77c4e29592162f7b603fd89d5524469de2597668a5c0ceabe94fa806b2bfe0658ef151102c8dc521908c89909fb34ff0815d07b0e4c893145c54a6bc63503d9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MemoBackbone::Application.config.session_store :cookie_store, key: '_memo_backbone_session'
